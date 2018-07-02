e2bcaaca5784f225c69f762e0212ef260b4879a0
sweida <weidaxyy@163.com>
2018-07-03 02:25:09 +0800

    mint-ui rem适配

---
+++ build/HelloWorld.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="hello">
+    <van-nav-bar
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    />
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <!-- <van-address-list
+      v-model="chosenAddressId"
+      :list="list"
+      @add="onAdd"
+      @edit="onEdit"
+    />
+    <van-cell-group> -->
+      <van-field
+        v-model="username"
+        required
+        clearable
+        label="用户名"
+        icon="question"
+        placeholder="请输入用户名"
+        @click-icon="$toast('question')"
+      />
+
+      <van-field
+        v-model="password"
+        type="password"
+        label="密码"
+        placeholder="请输入密码"
+        required
+      />
+    </van-cell-group>
+    <van-picker :columns="columns" @change="onChange" />
+
+    <van-goods-action>
+      <van-goods-action-mini-btn icon="chat" text="客服" @click="onClickMiniBtn" />
+      <van-goods-action-mini-btn icon="cart" text="购物车" @click="onClickMiniBtn" />
+      <van-goods-action-big-btn text="加入购物车" @click="onClickBigBtn" />
+      <van-goods-action-big-btn text="立即购买" @click="onClickBigBtn" primary />
+    </van-goods-action>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      chosenAddressId: '1',
+      columns: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
+      list: [
+        {
+          id: '1',
+          name: '张三',
+          tel: '13000000000',
+          address: '浙江省杭州市西湖区文三路 138 号东方通信大厦 7 楼 501 室'
+        },
+        {
+          id: '2',
+          name: '李四',
+          tel: '1310000000',
+          address: '浙江省杭州市拱墅区莫干山路 50 号'
+        }
+      ]
+    }
+  },
+  methods: {
+    // onAdd() {
+    //   Toast('新增收货地址');
+    // },
+    // onEdit(item, index) {
+    //   Toast('编辑收货地址:' + index);
+    // },
+    onChange(picker, value, index) {
+      this.$toast(`当前值：${value}, 当前索引：${index}`);
+    },
+    onClickMiniBtn() {
+      Toast('点击图标');
+    },
+    onClickBigBtn() {
+      Toast('点击按钮');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <div class="content">
+      <mt-button type="danger" size="large" plain>danger</mt-button>
+    </div>
+    <!-- <mt-button type="default" plain>default</mt-button>
+    <mt-button type="primary" plain>primary</mt-button> -->
+
+
+    <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+    <mt-picker :slots="slots" @change="onValuesChange"></mt-picker>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onValuesChange(picker, values) {
+      if (values[0] > values[1]) {
+        picker.setSlotValue(1, values[0]);
+      }
+    }
+  },
+  data() {
+    return {
+      slots: [
+        {
+          flex: 1,
+          values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
+          className: 'slot1',
+          textAlign: 'right'
+        }, {
+          divider: true,
+          content: '-',
+          className: 'slot2'
+        }, {
+          flex: 1,
+          values: ['2015-01', '2015-02', '2015-03', '2015-04', '2015-05', '2015-06'],
+          className: 'slot3',
+          textAlign: 'left'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content{
+  width:80%;
+  margin: auto;
+}
+</style>
